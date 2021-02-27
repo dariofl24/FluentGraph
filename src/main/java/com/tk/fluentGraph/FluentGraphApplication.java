2cc9c3a5412ed9f5bc508001cec745ddce60b26b
dflores <>
2021-02-27 16:05:20 -0600

    Adds base

---
+++ src/main/java/com/tk/fluentGraph/FluentGraphApplication.java
@@ -0,0 +1,13 @@
+package com.tk.fluentGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluentGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluentGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tk/fluentGraph/FluentGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tk.fluentGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluentGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
